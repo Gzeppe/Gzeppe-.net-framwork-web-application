bc3e58ba28f2679115531dccec47eb24c06dd69e
Swag <Swag@SWAG-PC>
2022-08-07 00:48:19 -0400

    Add project files.

---
+++ Vidley5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidley5.Startup))]
+namespace Vidley5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
